19bb13040e312fa2ad15f7d7e4e86c24b73af725
Chris Bitler <crb2547@rit.edu>
2017-09-17 11:19:13 -0400

    Add LAN page for EGS Legends: LAN of the Wild (#5)

---
+++ src/AppBundle/Controller/Events/LANController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace AppBundle\Controller\Events;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Controller for the LANs page
+ * @author Christopher Bitler <webmaster@egsrit.com>
+ */
+class LANController extends Controller
+{
+
+    /**
+     * Return the Symfony response for the page
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function indexAction(Request $request)
+    {
+        return $this->render('events/lanofthewild.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+            'top_type' => 'events',
+            'sub_type' => 'lans',
+        ]);
+    }
+}
