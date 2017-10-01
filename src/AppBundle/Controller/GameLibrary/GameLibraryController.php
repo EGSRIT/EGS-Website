6c5660214fd9ff0d0f50ea432136e65c3c493a48
Chris Bitler <crb2547@rit.edu>
2017-10-01 16:46:27 -0400

    Add game library test

---
+++ src/AppBundle/Controller/GameLibrary/GameLibraryController.php
@@ -0,0 +1,30 @@
+<?php
+
+
+namespace AppBundle\Controller\Membership;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Controller for the membership page
+ * @author Christopher Bitler <webmaster@egsrit.com>
+ */
+class GameLibraryController extends Controller
+{
+
+    /**
+     * Return the Symfony response for the page
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function indexAction(Request $request)
+    {
+        return $this->render('about/gamelibrary.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+            'top_type' => 'membership',
+            'sub_type' => '',
+        ]);
+    }
+}
