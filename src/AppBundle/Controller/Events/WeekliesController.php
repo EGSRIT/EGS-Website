b25a0b6c80c00fd85c33a341c1a4efda8de80ec4
Chris Bitler <crb2547@rit.edu>
2018-03-16 22:42:02 -0400

    Update way of adding weekly images

--- src/AppBundle/Controller/Events/WeekliesController.php
+++ src/AppBundle/Controller/Events/WeekliesController.php
@@ -23,6 +23,26 @@ class WeekliesController extends Controller
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
             'top_type' => 'events',
             'sub_type' => 'weeklies',
+            'pictures_upcoming' => $this->picturesUpcoming(),
+            'pictures_past' => $this->picturesPast()
         ]);
+    }
+
+    private function picturesUpcoming()
+    {
+        return array(
+            ['img' => 'Weekly-Left-4-Dead-2.png'],
+            ['img' => 'Weekly-Snipperclips-PNG.png']
+        );
+    }
+
+    private function picturesPast()
+    {
+        return array(
+            ['img' => 'Weekly-Screencheat.png'],
+            ['img' => 'Weekly-DBFZ-PNG.png'],
+            ['img' => 'Weekly-Dating-Sim-PNG.png'],
+            ['img' => 'Weekly-IDARB-PNG.png'],
+        );
     }
 }
